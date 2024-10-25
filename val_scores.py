c0723bd74992a1d22f099716bb4f47a99a15e78d
Avala Sri Karthik <srikarthikavala@gmail.com>
2024-10-26 03:30:41 +0530

    Update val_scores.py

--- val_scores.py
+++ val_scores.py
@@ -133,7 +133,7 @@ def generate_metrics_report(y_true, y_pred):
 
 # Define data transformations
 data_transforms = transforms.Compose([
-    transforms.Resize((224, 224)),  # Adjust size as needed
+    transforms.Resize((280, 280)),  # Adjust size as needed
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Example normalization for pre-trained models
 ])
